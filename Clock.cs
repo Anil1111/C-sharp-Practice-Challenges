99aa59638d0d0ef8cbcd3ff126247e6e41b85b01
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-29 18:34:49 +0200

    Create Clock.cs

---
+++ Clock.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Clock {
+    private int hours, minutes;
+
+    public Clock (int _hours, int _minutes) {
+        hours = _hours;
+        minutes = _minutes;
+        while (minutes < 0) {
+            minutes = 60 + minutes;
+            hours--;
+        }
+        while (hours < 0)
+            hours = 24 + hours;
+        var h = minutes / 60;
+        minutes = minutes - (h * 60);
+
+        hours = (hours + h) % 24;
+    }
+
+    public Clock Add (int minutesToAdd) => new Clock (hours, minutes + minutesToAdd);
+
+    public Clock Subtract (int minutesToSubtract) => new Clock (hours, minutes - minutesToSubtract);
+    public override string ToString () => String.Format ("{0:00}:{1:00}", hours, minutes);
+    public override bool Equals (object other) => (((Clock) other).minutes == this.minutes && ((Clock) other).hours == this.hours);
+}
