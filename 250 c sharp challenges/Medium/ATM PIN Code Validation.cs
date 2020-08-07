a6976ecb34701bfa7b2d7543928e7796540b3820
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-07 11:27:27 +0200

    update

---
+++ 250 c sharp challenges/Medium/ATM PIN Code Validation.cs
@@ -0,0 +1,123 @@
+/*
+
+ATM PIN Code Validation
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. Your task is to create a function that takes a string and returns true if the PIN is valid and false if it's not.
+
+Examples
+ValidatePIN("1234") ➞ true
+
+ValidatePIN("12345") ➞ false
+
+ValidatePIN("a234") ➞ false
+
+ValidatePIN("") ➞ false
+Notes
+Some test cases contain special characters.
+Empty strings must return false.
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+Solution 1
+
+*/
+
+using System.Linq;
+
+public class Program 
+{
+    public static bool ValidatePIN(string pin) 
+    {
+		  return  (pin.Length == 4 || pin.Length == 6) && pin.All(char.IsDigit);    
+    }
+}
+
+
+/*
+
+Solution 2
+
+*/
+
+
+
+public class Program 
+{
+    public static bool ValidatePIN(string pin) 
+    {
+      return (pin.Length == 4 || pin.Length == 6) && pin.All(char.IsDigit);
+    }
+}
+
+
+/*
+
+Solution 3
+
+*/
+public class Program 
+{
+    public static bool ValidatePIN(string pin) 
+    {
+      if(pin.Length != 4 && pin.Length != 6)
+        return false;
+      foreach(char c in pin)
+      {
+        if(c < 48 || c > 57)
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+}
+
+
+
+
+/*
+
+Solution 4
+
+*/
+
+
+public class Program 
+{
+    public static bool ValidatePIN(string pin) 
+    {
+      if (pin?.Length == 0 || pin.Length == 5) return false;
+
+            return pin.All(char.IsDigit);
+    }
+}
+
+
+
+
+public class Program 
+{
+    public static bool ValidatePIN(string pin) 
+    {
+      if(pin.Length < 4 || pin.Length == 5 || pin.Length > 6)
+				return false;
+			
+			for(int i=0;i<pin.Length;i++){
+				if(System.Char.IsDigit(pin[i])){
+					
+				}
+				else{
+					return false;
+				}
+			}
+			return true;
+    }
+}
---
+++ 250 c sharp challenges/Medium/Check if a Number is a Palindrome.cs
@@ -0,0 +1,107 @@
+/*
+
+Check if a Number is a Palindrome
+Create a function that returns true if a number is a palindrome.
+
+Examples
+IsPalindrome(838) ➞ true
+
+IsPalindrome(4433) ➞ false
+
+IsPalindrome(443344) ➞ true
+Notes
+A palindrome is a number that remains the same when reversed.
+Bonus: Try solving this without turning the number into a string.
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+Solution 1
+
+*/
+
+
+using System.Linq;
+
+public class Program
+{
+    public static bool IsPalindrome(int num)
+    {
+		return num.ToString() == string.Concat(num.ToString().Reverse().ToArray());
+    }
+}
+
+/*
+
+Solution 2
+
+*/
+
+public class Program
+{
+    public static bool IsPalindrome(int num)
+    {
+			 int remainder,sum=0,temp=num;     
+          while(num>0)      
+          {      
+           remainder=num%10;      
+           sum=(sum*10)+remainder;      
+           num=num/10;      
+          }      
+       if(temp==sum)
+				return true;
+			return false;
+    }
+}
+
+
+
+/*
+
+Solution 3
+
+*/
+
+public class Program
+{
+    public static bool IsPalindrome(int num)
+    {
+        string str = num.ToString();
+        for (int i = 0; i <= str.Length/2; i++)
+        {
+            if (!(str[i] == str[str.Length-1-i]))
+            {
+                return false;
+            }
+        }
+        return true;			
+    }
+}
+
+/*
+
+Solution 4
+
+*/
+
+
+public class Program {
+	public static bool IsPalindrome(int num) {
+		string str = num.ToString();
+		for (int i = 0; i < str.Length; i += 1) {
+			if (str[i] != str[str.Length - i - 1]) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
