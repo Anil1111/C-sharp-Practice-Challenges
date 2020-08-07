bb2511d27080589ec9d30f45a3e76be91520a06a
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-07 11:30:40 +0200

    update

---
+++ 250 c sharp challenges/Medium/Filter out Strings from an Array.cs
@@ -0,0 +1,149 @@
+/*
+Filter out Strings from an Array
+Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+
+Examples
+FilterArray([1, 2, "a", "b"]) ➞ [1, 2]
+
+FilterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+
+FilterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+Notes
+Zero is a non-negative integer.
+
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+Solution 1
+
+*/
+using System.Linq;
+
+public class Program
+    {
+        public static int[] FilterArray(object[] arr)
+        {
+            return arr.OfType<int>().ToArray();
+        }
+    }
+
+
+
+/*
+
+Solution 2
+
+*/
+
+
+
+public class Program
+{
+	public static int[] FilterArray(object[] arr) => arr.OfType<int>().ToArray();
+}
+
+/*
+
+Solution 3
+
+*/
+public class Program
+    {
+        public static int[] FilterArray(object[] arr)
+          => arr.Where(i=>i is int).Cast<int>().ToArray();
+    }
+
+
+/*
+
+Solution 4
+
+*/
+
+
+public class Program
+    {
+        public static int[] FilterArray(object[] arr)
+        {
+            var ints = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] is Int32)
+                {
+                    ints.Add((int)arr[i]);
+                }
+            }
+            return ints.ToArray();
+        }
+    }
+
+
+
+
+    public class Program
+    {
+        public static int[] FilterArray(object[] arr)
+        {
+            int count = 0;
+          	foreach(object current in arr)
+              if(current is int)
+                count++;
+          	int[] result = new int[count];
+          	count = 0;
+          	foreach(object current in arr)
+              if(current is int)
+              	result[count++] = (int)current;
+          	return result;
+        }
+    }
+
+
+
+public class Program
+    {
+        public static int[] FilterArray(object[] arr)
+        {
+					return arr.Where(a => a.GetType() == typeof(int)).Cast<int>().ToArray();
+        }
+    }
+
+
+
+
+    public class Program
+    {
+        public static int[] FilterArray(object[] arr)
+        {
+            return arr.OfType<int>().ToArray();
+        }
+    }
+
+
+
+
+public class Program
+    {
+        public static int[] FilterArray(object[] arr)
+        {
+            var filteredList = new List<int>();
+            int value = 0;
+            foreach (var item in arr)
+            {
+                if (int.TryParse(item.ToString(), out value))
+                {
+                    filteredList.Add(value);
+                }
+            }
+
+            return filteredList.Distinct().ToArray();
+        }
+    }
---
+++ 250 c sharp challenges/Medium/Return the Highest and Lowest Numbers.cs
@@ -0,0 +1,119 @@
+/*
+Return the Highest and Lowest Numbers
+Create a function that accepts a string of space separated numbers and returns the highest and lowest number (as a string).
+
+Examples
+HighLow("1 2 3 4 5") ➞ "5 1"
+
+HighLow("1 2 -3 4 5") ➞ "5 -3"
+
+HighLow("1 9 3 4 -5") ➞ "9 -5"
+
+HighLow("13") ➞ "13 13"
+Notes
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+Solution 1
+
+*/
+
+using System.Linq;
+public class Program
+{
+    public static string HighLow(string str) 
+		=> $"{str.Split(' ').Select(a=>int.Parse(a)).Max()} {str.Split(' ').Select(a=>int.Parse(a)).Min()}";
+}
+
+
+/*
+
+Solution 2
+
+*/
+
+public class Program 
+{
+    public static string HighLow(string str) 
+    {
+      List<int> ints = new List<int>();
+      List<string> listStrLineElements = str.Split(' ').ToList();
+      foreach (string number in listStrLineElements){
+        ints.Add(int.Parse(number));
+      }
+      // go through number list now and take out min and max
+      int min =ints[0];
+      int max = ints[0];
+      foreach(int num in ints){
+        if (min > num){
+          min =num;
+        }
+        else if (max < num){
+          max = num;
+        }
+        }
+    return max.ToString() + " " + min.ToString();
+    }
+    }
+
+
+
+
+/*
+
+Solution 3
+
+*/
+
+
+public class Program 
+{
+    public static string HighLow(string str) 
+    {
+      var nums = str.Split(' ').Select(s => int.Parse(s));
+      return $"{nums.Max()} {nums.Min()}";
+    }
+}
+
+
+
+
+
+public class Program 
+{
+    public static string HighLow(string str) 
+    {
+      var arr = str.Split(' ').Select(int.Parse).ToArray();
+      return arr.Max() + " " + arr.Min();
+    }
+}
+
+
+
+/*
+
+Solution 4
+
+*/
+
+
+public class Program 
+{
+    public static string HighLow(string str) 
+    {
+  			var ints = str.Split(' ').Select(s => int.Parse(s));
+      	return ints.Max() + " " + ints.Min();
+    }
+}
