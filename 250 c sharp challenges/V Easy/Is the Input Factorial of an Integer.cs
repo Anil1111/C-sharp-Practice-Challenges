5be57ae01feaf7afa49a83b865da041c72e17818
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-21 11:42:49 +0200

    Create edabit V Easy challenges

---
+++ 250 c sharp challenges/V Easy/Is the Input Factorial of an Integer.cs
@@ -0,0 +1,65 @@
+/*
+
+Is the Input Factorial of an Integer?
+Create a function that checks if a given integer is exactly the factorial of an integer or not. true if it is, false otherwise.
+
+Examples
+isFactorial(2) ➞ true
+// 2 = 2 * 1 = 2!
+
+isFactorial(27) ➞ false
+
+isFactorial(24) ➞ true
+// 24 = 4 * 3 * 2 * 1 = 4!
+Notes
+No error handling is necessary. Inputs are all positive integers.
+Alternatively, you can solve this with a recursive approach.
+
+*/
+
+
+
+
+
+
+//#############################################################
+//#                        MY SOLUTIONS                       #
+//#############################################################
+
+
+
+
+
+
+/*
+
+Solution 1
+
+*/
+
+
+
+
+/*
+
+Solution 2
+
+*/
+
+
+
+
+
+/*
+
+Solution 3
+
+*/
+
+
+
+/*
+
+Solution 4
+
+*/
---
+++ 250 c sharp challenges/V Easy/Perfect Number.cs
@@ -0,0 +1,66 @@
+/*
+
+Perfect Number
+Create a function that tests whether or not an integer is a perfect number. A perfect number is a number that can be written as the sum of its factors, excluding the number itself.
+
+For example, 6 is a perfect number, since 1 + 2 + 3 = 6, where 1, 2, and 3 are all factors of 6. Similarly, 28 is a perfect number, since 1 + 2 + 4 + 7 + 14 = 28.
+
+Examples
+CheckPerfect(6) ➞ true
+
+CheckPerfect(28) ➞ true
+
+CheckPerfect(496) ➞ true
+
+CheckPerfect(12) ➞ false
+
+CheckPerfect(97) ➞ false
+
+*/
+
+
+
+
+
+
+//#############################################################
+//#                        MY SOLUTIONS                       #
+//#############################################################
+
+
+
+
+
+
+/*
+
+Solution 1
+
+*/
+
+
+
+
+/*
+
+Solution 2
+
+*/
+
+
+
+
+
+/*
+
+Solution 3
+
+*/
+
+
+
+/*
+
+Solution 4
+
+*/
